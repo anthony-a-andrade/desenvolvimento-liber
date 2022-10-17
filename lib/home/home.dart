247881582f9390c714478669b4a71edd7595e5d9
Henrique <henriquearaujo1998@gmail.com>
2022-10-17 20:22:08 -0300

    segundo commit

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,232 +1,90 @@
+import 'package:demo_alternar_telas/home/list.dart';
 import 'package:flutter/material.dart';
 
-class Homepage extends StatefulWidget {
+class DemoHome extends StatefulWidget {
+  const DemoHome({Key? key}) : super(key: key);
+
   @override
-  State<Homepage> createState() => _HomepageState();
+  State<DemoHome> createState() => _DemoHomeState();
 }
 
-class _HomepageState extends State<Homepage> {
-  final lista = [
-    {
-      'nomeLivro': 'H.P. Lovecraft - Miskatonic Edition',
-      'imgLivro':
-          'https://darkside.vteximg.com.br/arquivos/ids/168103-519-519/95-hp-lovecraft-medo-classico-miskatonic-edition-1.jpg?v=636802549118500000',
-      'valorLivro': '35,90',
-      'distancia': 'Próximo de você',
-      'vendedor': 'Ana Banana',
-      'tema': 'terror',
-      'class': 'suspense'
-    },
-    {
-      'nomeLivro': 'Um Estudo em Vermelho ',
-      'imgLivro': 'https://imgs.extra.com.br/1519392636/1xg.jpg?imwidth=500',
-      'valorLivro': '15,90',
-      'distancia': 'Próximo de você',
-      'vendedor': 'João Mamão',
-      'tema': 'Mistério',
-      'class': 'aventura'
-    },
-    {
-      'nomeLivro': 'H.P. Lovecraft - Miskatonic Edition',
-      'imgLivro':
-          'https://darkside.vteximg.com.br/arquivos/ids/168103-519-519/95-hp-lovecraft-medo-classico-miskatonic-edition-1.jpg?v=636802549118500000',
-      'valorLivro': '35,90',
-      'distancia': 'Próximo de você',
-      'vendedor': 'Ana Banana',
-      'tema': 'terror',
-      'class': 'suspense'
-    },
-  ];
+class _DemoHomeState extends State<DemoHome> {
+  double horizontalPadding = 8, verticalPadding = 3;
+  int? botaoSelecionado = 1;
+  Widget? telaSelecionada = ListPage();
+  Map<int, Widget> telas = {
+    1: ListPage(),
+    2: Column(children: const [
+      Text("2"),
+    ]),
+    3: Column(children: const [
+      Text("3"),
+    ]),
+    4: Column(children: const [
+      Text("4"),
+    ]),
+  };
+  ScrollController scrollController = ScrollController();
+
+  Widget RoundedButton(int botaoSelecionado, IconData icon) {
+    Function()? onPressed;
+    Color iconColor = Colors.black;
+
+    if (botaoSelecionado != this.botaoSelecionado) {
+      onPressed = () {
+        setState(() {
+          this.botaoSelecionado = botaoSelecionado;
+          telaSelecionada = telas[botaoSelecionado];
+          scrollController.jumpTo(0);
+        });
+      };
+
+      iconColor = const Color.fromARGB(255, 74, 74, 74);
+    }
+
+    return TextButton(
+        onPressed: onPressed,
+        style: ButtonStyle(
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18.0)))),
+        child: Padding(
+            padding: EdgeInsets.fromLTRB(horizontalPadding, verticalPadding,
+                horizontalPadding, verticalPadding),
+            child: Icon(icon, color: iconColor, size: 30)));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              margin: const EdgeInsets.only(top: 20, left: 10, bottom: 20),
-              alignment: Alignment.centerLeft,
-              child: const Text(
-                'Descobertas da Semana!',
-                style: TextStyle(fontSize: 25),
-              ),
-            ),
-            Container(
-              height: 350,
-              width: MediaQuery.of(context).size.width - 25,
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-                color: Colors.blue,
-              ),
-              child: Column(
-                children: [
-                  Container(
-                    margin: const EdgeInsets.only(
-                        left: 10, right: 10, top: 10, bottom: 10),
-                    height: 200,
-                    color: Colors.black,
-                  ),
-                  const Text("MARX, uma introdução"),
-                  const Text("200,00")
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 600,
-              child: ListView.builder(
-                itemCount: lista.length,
-                itemBuilder: (context, i) {
-                  var infor = lista[i];
-                  return Container(
-                    height: 155,
-                    width: MediaQuery.of(context).size.width - 25,
-                    decoration: const BoxDecoration(
+        backgroundColor: const Color.fromARGB(255, 202, 202, 202),
+        body: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(children: [
+              Expanded(
+                  child: Container(
+                color: const Color.fromARGB(255, 255, 255, 255),
+                child: SizedBox.expand(
+                    child: SingleChildScrollView(
+                        controller: scrollController, child: telaSelecionada)),
+              )),
+              const Padding(padding: EdgeInsets.all(4)),
+              Container(
+                  decoration: const BoxDecoration(
                       color: Colors.white,
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(4),
-                      ),
-                      boxShadow: [
-                        BoxShadow(color: Color(0xFFECEFF1), blurRadius: 8)
-                      ],
-                    ),
-                    child: Column(
-                      children: [
-                        Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
+                      borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(20),
+                          bottomRight: Radius.circular(20))),
+                  child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: [
-                            Container(
-                              margin: const EdgeInsets.only(left: 10, top: 10),
-                              height: 90,
-                              width: 100,
-                              decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  image: DecorationImage(
-                                      fit: BoxFit.fill,
-                                      image: NetworkImage(infor['imgLivro']!))),
-                            ),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Container(
-                                  margin: const EdgeInsets.only(top: 10),
-                                  width:
-                                      MediaQuery.of(context).size.width - 150,
-                                  child: Text(
-                                    infor['nomeLivro']!,
-                                    style: const TextStyle(fontSize: 21),
-                                  ),
-                                ),
-                                Row(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceAround,
-                                  children: [
-                                    Container(
-                                      margin: const EdgeInsets.only(
-                                          top: 15, left: 5),
-                                      child: Text(
-                                        infor['valorLivro']!,
-                                        style: const TextStyle(fontSize: 26),
-                                      ),
-                                    ),
-                                    const SizedBox(
-                                      width: 100,
-                                    ),
-                                    Container(
-                                      margin: const EdgeInsets.only(
-                                        top: 20,
-                                      ),
-                                      child: Text(infor['distancia']!,
-                                          style: const TextStyle(
-                                              fontSize: 18,
-                                              color: Color(0xFF78909C))),
-                                    ),
-                                  ],
-                                ),
-                                Row(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceAround,
-                                  children: [
-                                    Container(
-                                      margin: const EdgeInsets.only(
-                                          left: 5, top: 5),
-                                      child: const Text(
-                                        'Vendido por:',
-                                        style: TextStyle(
-                                            fontSize: 18,
-                                            color: Color(0xFF78909C)),
-                                      ),
-                                    ),
-                                    Container(
-                                      margin: const EdgeInsets.only(
-                                          left: 5, top: 5),
-                                      child: Text(infor['vendedor']!,
-                                          style: const TextStyle(
-                                              fontSize: 18,
-                                              color: Color(0xFFE265D8))),
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            Container(
-                              margin: const EdgeInsets.only(left: 15, top: 12),
-                              padding: const EdgeInsets.only(
-                                  top: 5, bottom: 5, left: 8, right: 8),
-                              height: 20,
-                              decoration: const BoxDecoration(
-                                color: Color(0xFFE43D61),
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(10),
-                                ),
-                              ),
-                              alignment: Alignment.center,
-                              child: Text(
-                                infor['tema']!.toUpperCase(),
-                                style: const TextStyle(
-                                    color: Colors.white, fontSize: 10),
-                              ),
-                            ),
-                            const SizedBox(
-                              width: 8,
-                            ),
-                            Container(
-                              margin: const EdgeInsets.only(top: 12),
-                              padding: const EdgeInsets.only(
-                                  top: 5, bottom: 5, left: 8, right: 8),
-                              height: 20,
-                              decoration: const BoxDecoration(
-                                color: Color(0xFF4A4A4A),
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(10),
-                                ),
-                              ),
-                              alignment: Alignment.center,
-                              child: Text(
-                                infor['class']!.toUpperCase(),
-                                style: const TextStyle(
-                                    color: Colors.white, fontSize: 10),
-                              ),
-                            ),
-                            const SizedBox(
-                              height: 8,
-                            ),
-                          ],
-                        )
-                      ],
-                    ),
-                  );
-                },
-              ),
-            )
-          ],
-        ),
-      ),
-    );
+                            RoundedButton(1, Icons.home),
+                            RoundedButton(2, Icons.shopping_basket),
+                            RoundedButton(3, Icons.folder_open),
+                            RoundedButton(4, Icons.chat_bubble)
+                          ])))
+            ])));
   }
 }
